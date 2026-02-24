f64836f406e46f896f1f25e065b6673d36b692c8
Caroline <caroonyango9@gmail.com>
2026-02-24 10:35:39 +0300

    Setting up the project and the app

---
+++ kianproject/kianapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KianappConfig(AppConfig):
+    name = 'kianapp'
---
+++ kianproject/kianapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path # helps us import the method path for our endpoints
+from . import views # import veiws from the current root directory
+
+    # endpoints + target view functions
+    
+urlpatterns = [
+    path('', views.home, name='home'),
+]
