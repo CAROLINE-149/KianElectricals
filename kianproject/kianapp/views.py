504ea3e85b135903b1b8b1fa40a1e2267da0e59f
Caroline <caroonyango9@gmail.com>
2026-02-25 04:30:03 +0300

    Creation of the super user and the templates

--- kianproject/kianapp/views.py
+++ kianproject/kianapp/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+#def produceSector(request): 
+   # return render(request, 'firstApp/home.html')  
+
 def home(request):
     return HttpResponse("Kian App is working")
